01ab5174670a6c26250dca872b204610e3fd2eb7
Desmond Obinna <talk2desmond91@gmail.com>
2024-05-10 01:30:06 +0100

    created a nodeJS app

---
+++ app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const os = require('os');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.url === '/') {
+    const cpuInfo = os.cpus();
+    const osInfo = os.platform()
+
+    const data = {
+      cpu: cpuInfo,
+      os: osInfo,
+    };
+    res.end(JSON.stringify(data));
+  } else {
+    
+
+    const delay = Math.floor(Math.random() * 2000);
+    setTimeout(() => {
+        if (req.method === 'OPTIONS') {
+            return res.end();
+          }
+        res.end("Hello from server!");
+    }, delay);
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
